47c6b32b33372b20fbae7ca8278dd89b6c96126d
Adam PM <adam.pm77@gmail.com>
2023-10-26 20:07:24 +0700

    perbaikan player management & score

--- app/Http/Controllers/PlayerManagementController.php
+++ app/Http/Controllers/PlayerManagementController.php
@@ -45,9 +45,10 @@ class PlayerManagementController extends Controller
         ]);
 
         $now = Carbon::now();
-        $late = Player::whereYear('created_at', $now)->orderBy('seq', 'desc')->limit(1)->first();
+        $late = Player::whereYear('created_at', $now)->orderBy('seq', 'desc')->limit(1)->get();
+        $late_count = Player::whereYear('created_at', $now)->orderBy('seq', 'desc')->limit(1)->count();
 
-        if ($late && $late->count() == 0) {
+        if ($late->count() == 0) {
             $seq = 1;
         } else {
             $seq  = $late->seq + 1;
