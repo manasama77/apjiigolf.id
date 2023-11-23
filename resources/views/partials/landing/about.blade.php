78cf95a35c60f456bc11cf1e873101882e057ea9
Adam PM <adam.pm77@gmail.com>
2023-11-23 10:56:01 +0700

    update about

--- resources/views/partials/landing/about.blade.php
+++ resources/views/partials/landing/about.blade.php
@@ -7,10 +7,11 @@
                 <div class="row">
                     <div class="col-sm-12 col-md-6 offset-md-3">
                         <p class="text-white mb-5">
-                            PGA is an abbreviation of "Persatuan Golf APJII". The PGA is responsible for organizing golf
-                            for APJII Member. It aims to develop and nurture the talents of golfers by providing
-                            competitive platforms and opportunities. Through its tournaments and initiatives, the PGA
-                            strives to establish a strong golfing community for APJII Member.
+                            PGA stands for <strong>"Persatuan Golf APJII"</strong> and it is responsible for organizing
+                            golf events and
+                            activities for all members of APJII. PGA aims to develop and nurture the talents of golfers
+                            by providing competitive platforms and opportunities. Through its tournaments and
+                            initiatives, PGA strives to establish a strong golfing community for APJII members.
                         </p>
                     </div>
                 </div>
