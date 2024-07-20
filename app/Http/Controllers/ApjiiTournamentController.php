bb272942820c65374b64a2da8bd5efa374e349a3
Adam PM <adam.pm77@gmail.com>
2024-07-21 05:39:21 +0700

    aaa

--- app/Http/Controllers/ApjiiTournamentController.php
+++ app/Http/Controllers/ApjiiTournamentController.php
@@ -87,7 +87,6 @@ class ApjiiTournamentController extends Controller
 
     public function index()
     {
-        dump(config('services.recaptcha.secret_key'));
         $event_name          = $this->event_name;
         $event_date          = $this->event_date;
         $event_time          = $this->event_time;
