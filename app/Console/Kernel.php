003333b4e407792c469d624034a1f8628b577bd6
Adam PM <adam.pm77@gmail.com>
2024-07-21 07:30:43 +0700

    update kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('apjii:status')->everyThirtySeconds()->withoutOverlapping()->emailOutputOnFailure('adam.pm77@gmail.com')->runInBackground();
+        $schedule->command('apjii:status')->everyMinute()->withoutOverlapping()->emailOutputOnFailure('adam.pm77@gmail.com')->runInBackground();
     }
 
     /**
