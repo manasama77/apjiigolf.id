40644e8e7ad3b24a92cb38f6cf9a417de278575c
Adam PM <adam.pm77@gmail.com>
2024-07-26 09:12:51 +0700

    update wording

--- resources/views/partials/landing/form_register_apjii_golf_tournament.blade.php
+++ resources/views/partials/landing/form_register_apjii_golf_tournament.blade.php
@@ -271,8 +271,8 @@
                                         <label for="code">Promo Code</label>
                                     </div>
                                     <p class="text-muted text-start mb-5">
-                                        If you are a member of APJII, please <a href="{{ $wa_pic }}"><i
-                                                class="fab fa-whatsapp"></i> contact us</a> to get a special promo
+                                        For APJII members, <a href="{{ $wa_pic }}">contact us</a> to get special
+                                        price.
                                     </p>
 
                                     <div class="row mb-3 justify-content-center">
