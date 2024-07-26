dd49cdda8363b5eb86ef6644bc13d05a29426ab5
Adam PM <adam.pm77@gmail.com>
2024-07-26 11:07:50 +0700

    add telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('apjii:status')->everyMinute()->withoutOverlapping()->emailOutputOnFailure('adam.pm77@gmail.com')->runInBackground();
+        $schedule->command('telescope:prune')->monthly();
     }
 
     /**
