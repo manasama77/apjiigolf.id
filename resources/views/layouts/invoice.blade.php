c120092a2e9bc332749f64739606cb589b4c0230
Adam PM <adam.pm77@gmail.com>
2024-07-26 09:47:57 +0700

    aaa

--- resources/views/layouts/invoice.blade.php
+++ resources/views/layouts/invoice.blade.php
@@ -3,70 +3,70 @@
 <style>
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-Thin.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-Thin.ttf') }}) format("truetype");
         font-weight: 100;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-ExtraLight.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-ExtraLight.ttf') }}) format("truetype");
         font-weight: 200;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-Light.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-Light.ttf') }}) format("truetype");
         font-weight: 300;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-Regular.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-Regular.ttf') }}) format("truetype");
         font-weight: 400;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-Medium.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-Medium.ttf') }}) format("truetype");
         font-weight: 400;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-Medium.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-Medium.ttf') }}) format("truetype");
         font-weight: 500;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-SemiBold.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-SemiBold.ttf') }}) format("truetype");
         font-weight: 600;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-Bold.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-Bold.ttf') }}) format("truetype");
         font-weight: 700;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-BoldItalic.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-BoldItalic.ttf') }}) format("truetype");
         font-weight: 800;
         font-style: normal; // use the matching font-style here
     }
 
     @font-face {
         font-family: 'Poppins';
-        src: url({{ storage_path('fonts\Poppins-Black.ttf') }}) format("truetype");
+        src: url({{ storage_path('fonts/Poppins-Black.ttf') }}) format("truetype");
         font-weight: 900;
         font-style: normal; // use the matching font-style here
     }
